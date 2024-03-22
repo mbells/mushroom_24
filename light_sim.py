ee8b26fa3d8095cfe395050e6185a6b7e97bd2bd
Matthew Bells <github@mbells.ca>
2024-03-22 12:19:40 -0400

    Crossed points
    Implement crossed points in light string.
    Parameterize crossed points.
    Add a locator to easily find address of each light.
    Adjust damping based on the number of inputs active.

--- light_sim.py
+++ light_sim.py
@@ -5,6 +5,8 @@ import numpy as np
 
 from scipy import interpolate
 
+import wavesim
+
 width = 1000
 height = 250
 num_channels = 3
@@ -13,7 +15,14 @@ num_channels = 3
 # 28 in high
 # cable: 16 ft ea, *2 sections
 
+
 num_points = 280
+source_0 = 0
+source_1 = num_points - 1
+DAMPING_HIGH = 0.005
+DAMPING_MED = 0.001
+DAMPING_LOW = 0.0001
+
 lightstring_original = np.array(
     [
         (100, 10),
@@ -36,6 +45,7 @@ lightstring_original = np.array(
         (125, 250),
     ]
 )
+crossed_points_original = [(119, 214)]
 
 lightstring_simple_2 = np.array(
     [
@@ -54,42 +64,126 @@ lightstring_simple_2 = np.array(
         (0, 125),
     ]
 )
+crossed_points_simple_2 = [(70, 210)]
 
 source_0 = 0
 source_1 = 140
 
 ctr_pts = lightstring_original
-#ctr_pts = lightstring_simple_2
+crossed_points = crossed_points_original
+# ctr_pts = lightstring_simple_2
+# crossed_points = crossed_points_simple_2
+
+lights = np.full((num_points, num_channels), (0, 0, 0), dtype=np.uint8)
 
 
+def light_waves(lights, wave0, wave1, wave2):
+    r = np.clip(100 + 200 * wave0.u, 0, 255)
+    g = np.clip(100 + 200 * wave1.u, 0, 255)
+    b = np.clip(100 + 200 * wave2.u, 0, 255)
+    lights[..., 0] = b
+    lights[..., 1] = g
+    lights[..., 2] = r
 
-def interpolate_points(control_pts, num_points):
-    x = control_pts[:, 0]
-    y = control_pts[:, 1]
-    tck, u = interpolate.splprep([x, y], k=3, s=0)
-    u = np.linspace(0, 1, num=num_points, endpoint=True)
-    out = interpolate.splev(u, tck)
-    return out
+
+class LightsSim:
+    def __init__(self, ctr_pts, num_points):
+        (self.lights_x, self.lights_y) = self.interpolate_points(ctr_pts, num_points)
+
+    def interpolate_points(self, control_pts, num_points):
+        x = control_pts[:, 0]
+        y = control_pts[:, 1]
+        tck, u = interpolate.splprep([x, y], k=3, s=0)
+        u = np.linspace(0, 1, num=num_points, endpoint=True)
+        out = interpolate.splev(u, tck)
+        return out
+
+    def draw(self, img, lights):
+        for i in range(num_points):
+            x, y = np.intp((self.lights_x[i], height - self.lights_y[i]))
+            c = lights[i].tolist()
+            cv2.circle(img, (x, y), radius=4, color=c, thickness=-1)
+
+
+def move_locator(locator, amount):
+    if locator is None:
+        return None
+    locator += amount
+    if locator < 0:
+        locator += num_points
+    if locator >= num_points:
+        locator -= num_points
+    print(locator)
+    return locator
 
 
 def main():
-    (lights_x, lights_y) = interpolate_points(ctr_pts, num_points)
+
+    lightssim = LightsSim(ctr_pts, num_points)
+
+    wave0 = wavesim.WaveSim(num_points, source=source_0, crossed_points=crossed_points)
+    wave1 = wavesim.WaveSim(num_points, source=source_1, crossed_points=crossed_points)
+    wave2 = wavesim.WaveSim(num_points, crossed_points=crossed_points)
 
     img = np.full((height, width, num_channels), (0, 0, 0), dtype=np.uint8)
+    locator = None
 
-    for i in range(num_points):
-        x, y = np.intp((lights_x[i], height-lights_y[i]))
-        #print(x, y)
-        cv2.circle(img, (x, y), radius=4, color=(0, 0, 255), thickness=-1)
+    t = 0
+    while True:
+        t += 1
+        wave0.update_wave(t)
+        wave1.update_wave(t)
+        wave2.update_wave(t)
+        light_waves(lights, wave0, wave1, wave2)
+        img *= 0
+        lightssim.draw(img, lights)
 
+        if locator is not None:
+            x, y = np.intp(
+                (lightssim.lights_x[locator], height - lightssim.lights_y[locator])
+            )
+            # print(x,y)
+            cv2.circle(img, (x, y), radius=6, color=(255, 255, 255), thickness=2)
 
-    cv2.imshow("lights", img)
+        cv2.imshow("lights", img)
 
-    # waitKey() waits for a key press to close the window and 0 specifies indefinite loop
-    cv2.waitKey(0)
+        key = cv2.waitKey(1)
+        if key == ord("q"):
+            cv2.destroyAllWindows()
+            break
+        elif key == ord("1"):
+            wave0.source_active = not wave0.source_active
+        elif key == ord("2"):
+            wave1.source_active = not wave1.source_active
+        elif key == ord("l"):
+            print("locator")
+            if locator is None:
+                locator = 0
+            else:
+                locator = None
+        elif key == -1:
+            pass
+        elif key == 151 or key == 82:  # Up
+            locator = move_locator(locator, 1)
+        elif key == 154 or key == 85:  # PgUp
+            locator = move_locator(locator, 10)
+        elif key == 153 or key == 84:  # Down
+            locator = move_locator(locator, -1)
+        elif key == 155 or key == 86:  # PageDown
+            locator = move_locator(locator, -10)
+        else:
+            print(f"Unknown key {key}")
 
-    # cv2.destroyAllWindows() simply destroys all the windows we created.
-    cv2.destroyAllWindows()
+        # Adjust simulation parameters...
+        if wave0.source_active and wave1.source_active:
+            wave0.damping_factor = DAMPING_LOW
+            wave1.damping_factor = DAMPING_LOW
+        elif wave0.source_active or wave1.source_active:
+            wave0.damping_factor = DAMPING_MED
+            wave1.damping_factor = DAMPING_MED
+        else:
+            wave0.damping_factor = DAMPING_HIGH
+            wave1.damping_factor = DAMPING_HIGH
 
 
 if __name__ == "__main__":
--- wavesim.py
+++ wavesim.py
@@ -5,70 +5,75 @@ import cv2
 import math
 
 # Parameters
-num_points = 140
-num_steps = 1000
-velocity = 0.2  # Velocity of the wave
+num_points = 280
+velocity = 0.1  # Velocity of the wave
 time_step = 1  # Time step
 x_step = 1
-damping_factor = 0.001  # Damping factor
-source_freq = 1/100
+cfg_damping_factor = 0.0001  # Damping factor
+source_freq = 1 / 100
 
 width = 1000
 height = 250
 num_channels = 3
 
-crossed_points=[]
-crossed_points=[(40, 100)]
+crossed_points = []
+#crossed_points = [(40, 100)]
+#crossed_points = [(119, 214)]
 
-#source_0 = int(num_points / 2)
+# source_0 = int(num_points / 2)
 source_0 = 0
 
+
 class WaveSim:
-    def __init__(self, num_points, source=0):
+    def __init__(self, num_points, source=None, crossed_points=[]):
         self.num_points = num_points
         self.source = source
+        self.crossed_points = crossed_points
+
         # Initial conditions
         self.x = np.arange(num_points)
         self.u = np.zeros(num_points)  # Initial displacement
         self.u_1 = np.zeros(num_points)  # Last step (u[t-1])
         self.u_next = np.zeros(num_points)  # Next step, used temporarily
 
-        self.rsq=(velocity*time_step/x_step)**2
-
+        self.rsq = (velocity * time_step / x_step) ** 2
+        self.source_active = False
+        self.damping_factor = cfg_damping_factor
 
     def update_wave(self, step):
         # Update displacement using wave equation
-        #u[1:-1] += rsq * (u[:-2] - 2 * u[1:-1] + u[2:])
+        # u[1:-1] += rsq * (u[:-2] - 2 * u[1:-1] + u[2:])
 
-        #global u, u_1, u_next, source_freq
+        # global u, u_1, u_next, source_freq
         u = self.u
         u_next = self.u_next
         u_1 = self.u_1
         rsq = self.rsq
 
-        for a in range(1, len(u)-1):
-            u_next[a] = 2*(1-rsq)*u[a]-u_1[a]+rsq*(u[a-1]+u[a+1])
+        for a in range(1, len(u) - 1):
+            u_next[a] = 2 * (1 - rsq) * u[a] - u_1[a] + rsq * (u[a - 1] + u[a + 1])
 
         np.clip(u_next, -1, 1, out=u_next)
 
         # Apply damping
-        u_next *= 1 - damping_factor * time_step
+        u_next *= 1 - self.damping_factor * time_step
         # Boundary conditions (fixed ends)
         u_next[0] = 0
         u_next[-1] = 0
 
         # When crosses are defined, these affect each other:
-        for a,b in crossed_points:
+        for a, b in self.crossed_points:
             avg = np.mean([u_next[a], u_next[b]])
             u_next[a] = u_next[b] = avg
 
-        #if np.isclose(u[self.source], 0, atol=0.01):
+        # if np.isclose(u[self.source], 0, atol=0.01):
         """
         if step % 100 == 0:
             u_next[self.source] = 1.0  # Repeat pulse
             print("pulse")
         """
-        u_next[self.source] = math.sin(2*math.pi*step*source_freq)
+        if self.source is not None and self.source_active:
+            u_next[self.source] = math.sin(2 * math.pi * step * source_freq)
 
         # Rotate; `next` will be overwritten but this avoids memory allocation
         u, u_1, u_next = u_next, u, u_1
@@ -83,46 +88,45 @@ class WaveSim:
         # Update displacement using wave equation
         u[1:-1] += rsq * (u[:-2] - 2 * u[1:-1] + u[2:])
 
-
-        # Refactoring   
+        # Refactoring
         tmp = u
-        u += 2*(1-rsq)*u[a] - u1[a] + rsq*(u[a-1]+u[a+1])
+        u += 2 * (1 - rsq) * u[a] - u1[a] + rsq * (u[a - 1] + u[a + 1])
 
-        u += 2*u[a] - 2*rsq*u[a] + rsq*(u[a-1]+u[a+1])  - u1[a]
+        u += 2 * u[a] - 2 * rsq * u[a] + rsq * (u[a - 1] + u[a + 1]) - u1[a]
 
         u1 = tmp
-        #u1, u = u, tmp
-
-
+        # u1, u = u, tmp
 
         # Apply damping
-        u *= 1 - damping_factor * time_step
+        u *= 1 - self.damping_factor * time_step
         # Boundary conditions (fixed ends)
         u[0] = 0
         u[-1] = 0
 
-
     def draw(self, img):
-        img *= 0
         for i in range(self.num_points):
-            x, y = np.intp((4*i, 100*self.u[i] + height/2))
+            x, y = np.intp((4 * i, 100 * self.u[i] + height / 2))
             cv2.circle(img, (x, y), radius=4, color=(0, 0, 255), thickness=-1)
 
 
-
 def main():
-    wavesim = WaveSim(num_points, source=source_0)
+    wavesim = WaveSim(num_points, source=source_0, crossed_points=crossed_points)
+    wavesim.source_active = True
     img = np.full((height, width, num_channels), (0, 0, 0), dtype=np.uint8)
-    for step in range(num_steps):
-        #print(f"===== {step}")
-        #if step == 1:
+    t = 0
+    while True:
+        t += 1
+        # print(f"===== {step}")
+        # if step == 1:
         #    print(u)
-        wavesim.update_wave(step)
+        wavesim.update_wave(t)
+        img *= 0
         wavesim.draw(img)
         cv2.imshow("lights", img)
-        if cv2.waitKey(1) == ord('q'):
+        if cv2.waitKey(1) == ord("q"):
             cv2.destroyAllWindows()
             break
+
 
 if __name__ == "__main__":
     main()
